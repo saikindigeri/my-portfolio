a452f1baa19200964c6a509e10c8749c8dc56ded
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-01-01 21:10:04 +0530

    added btn

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 import { Cover } from "@/components/ui/cover";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 
 
 const NewPage = () => {
@@ -15,6 +16,15 @@ const NewPage = () => {
           <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             Build amazing websites <br /> at <Cover>warp speed</Cover>
           </h1>
+
+          <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+      >
+        <AceternityLogo />
+        <span>Aceternity UI</span>
+      </HoverBorderGradient>
         </div>
       );
     }
@@ -24,3 +34,25 @@ const NewPage = () => {
 
 
 export default NewPage
+
+
+const AceternityLogo = () => {
+  return (
+    <svg
+      width="66"
+      height="65"
+      viewBox="0 0 66 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-3 w-3 text-black dark:text-white"
+    >
+      <path
+        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+        stroke="currentColor"
+        strokeWidth="15"
+        strokeMiterlimit="3.86874"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
