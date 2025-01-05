d8d701f76f251aab54bd8144b0ceadf11df08528
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-01-05 11:04:48 +0530

    done

--- my-app/config/nodemailer.js
+++ my-app/config/nodemailer.js
@@ -1,3 +1,5 @@
+/*
+
 import nodemailer from 'nodemailer'
 
 const email=process.env.EMAIL;
@@ -15,4 +17,25 @@ export const transporter=nodemailer.createTransport({
 export const mailOptions={
     from:email,
     to:email,
-}
+} 
+
+*/
+
+
+import nodemailer from "nodemailer";
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass,
+  },
+});
+
+export const mailOptions = {
+  from: email,
+  to: email,
+};
