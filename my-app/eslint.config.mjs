e18cb422e2fa43b5f92e6f113ffbcb23a19ef95a
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-01-07 10:35:48 +0530

    lsat

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,5 +1,3 @@
-/*
-
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -14,34 +12,5 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-
-export default eslintConfig;
- 
-
-*/
-
-
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import { createConfig } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = createConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["*.tsx", "*.ts"], // Apply this config to .ts and .tsx files
-    rules: {
-      // Your custom rules here
-      "react/prop-types": "off", // Example rule
-    },
-  },
-]);
 
 export default eslintConfig;
