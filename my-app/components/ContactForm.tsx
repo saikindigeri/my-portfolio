47d39e48ffa3b6e70358fa6b8ee6d980121d7fd2
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-01-07 06:27:35 +0530

    removed unused files

--- my-app/components/ContactForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {}
-
-const ContactForm = () => {
-  return <div></div>
-}
-
-export default ContactForm
--- my-app/components/ProjectCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {}
-
-const ProjectCard = () => {
-  return <div></div>
-}
-
-export default ProjectCard
