15d3f9e09225a364e2b488ae0a1fdc8497b04c79
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-01-29 09:27:58 +0530

    added py and changed hover effect

--- my-app/app/skills/page.tsx
+++ my-app/app/skills/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import "@fontsource/poppins"; // Import Poppins font
-import { SiJavascript, SiReact, SiNextdotjs, SiNodedotjs, SiBlockchaindotcom, SiTailwindcss, SiTypescript, SiMongodb, SiMysql } from "react-icons/si";
+import { SiJavascript, SiReact, SiNextdotjs, SiNodedotjs, SiPython, SiTailwindcss, SiTypescript, SiMongodb, SiMysql } from "react-icons/si";
 
 const skills = [
   { name: "JavaScript", icon: <SiJavascript size={40} /> },
   { name: "React", icon: <SiReact size={40} /> },
   { name: "Next.js", icon: <SiNextdotjs size={40} /> },
   { name: "Node.js", icon: <SiNodedotjs size={40} /> },
-  { name: "Blockchain", icon: <SiBlockchaindotcom size={40} /> },
+  { name: "Python", icon: <SiPython size={40} /> },
   { name: "CSS & Tailwind", icon: <SiTailwindcss size={40} /> },
   { name: "TypeScript", icon: <SiTypescript size={40} /> },
   { name: "MongoDB", icon: <SiMongodb size={40} /> },
@@ -26,9 +26,9 @@ export function Skills() {
     {skills.map((skill) => (
       <div
         key={skill.name}
-        className="relative p-8 rounded-lg shadow-xl bg-white/10 backdrop-blur-lg border border-white/10 hover:bg-gradient-to-br from-blue-950 to-rose-900 hover:scale-110 transition-all duration-300 group"
+        className="relative p-8 rounded-lg shadow-xl bg-white/10 backdrop-blur-lg border border-white/10 hover:bg-gradient-to-br from-black to-purple-900 hover:scale-110 transition-all duration-300 group"
       >
-        <div className="absolute inset-0 z-0 rounded-lg bg-gradient-to-b from-transparent via-white/10 to-transparent group-hover:via-white/20"></div>
+        <div className="absolute inset-0 z-0 rounded-lg bg-gradient-to-b from-transparent via-white/30 to-transparent group-hover:via-white/20"></div>
 
         <div className="relative z-10 flex flex-col items-center justify-center">
           {skill.icon}
